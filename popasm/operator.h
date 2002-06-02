4eea422d55c53fb5c20beeac8aebd0606c46a09e
helcio <helcio>
2002-06-02 14:21:29 +0000

    Created class EncloserData which inheriths from OperatorData. I guess things will be more organized that way.

--- popasm/operator.h
+++ popasm/operator.h
@@ -6,6 +6,10 @@
     email                : helcio@users.sourceforge.net
  ***************************************************************************/
 
+//--------------------------------------------------------------------------
+// Templates to model operators' behavior
+//--------------------------------------------------------------------------
+
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -20,40 +24,70 @@
 
 #include <string>
 #include <exception>
+#include "defs.h"
 
 class OperatorException;
 class InvalidArity;
+class InvalidUsage;
 
 // Contains all data needed to describe an operator
 template <class T = int>
 class OperatorData
 {
-	string Name;					// Operator name (eg. "/")
-	unsigned int Precedence;	// Operator precedence. The higher the number the higher the precedence
+	// Operator name (eg. "/")
+	string Name;
+
+	// Operator precedences for unary prefix, unary postfix and binary usages, in this order.
+	// The higher the number the higher the precedence. Precedence zero means the operator cannot
+	// be used in the corresponding way. Eg.: if Precedence[PREFIX] == 0, then the operator cannot
+	// be used as a unary prefix one.
+	unsigned int Precedences[2];
 
 	public:
-	OperatorData (const string &n, unsigned int p) throw () : Name(n), Precedence(p) {}
+	OperatorData (const string &n, unsigned int p0, unsigned int p1) throw ()
+		: Name(n) {Precedences[0] = p0; Precedences[1] = p1;}
 	virtual ~OperatorData () throw () {}
 
 	// Unary prefix
 	virtual T &operator() (T &x) const throw (InvalidArity) {throw InvalidArity(Name, "unary prefix");}
-	// Unary postfix
-	virtual T &operator() (T &x, int) const throw (InvalidArity) {throw InvalidArity(Name, "unary postfix");}
 	// Binary
 	virtual T &operator() (T &x, T &y) const throw (InvalidArity) {throw InvalidArity(Name, "binary");}
 
 	// Returns operator name
-	const string &GetName () const {return Name;};
-	// Checks if (*this) has higher precedence than op
-	bool Precedes (const OperatorData &op) const throw () {return Precedence > op.Precedence;}
+	const string &GetName () const throw () {return Name;};
+
+	// Returns the precedence for the given usage
+	enum Usage {PREFIX = 0, BINARY = 1};
+	unsigned int Precedence (Usage u) const throw (InvalidUsage)
+	{
+		if (u > 1) throw InvalidUsage(u);
+		return Precedences[u];
+	}
+};
+
+// Extends OperatorData, including a pointer to the typeinfo object that describes the closing encloser
+template <class T = int>
+class EncloserData : public OperatorData<T>
+{
+	// Operator that closes the encloser
+	string Closer;
+
+	public:
+	EncloserData (const string &n, const string &c = "") : OperatorData<T> (n, 0, 0), Closer (c) {}
+	~EncloserData () {}
+
+	// Checks if the encloser is an opening one (such as '[' or '(')
+	bool Opens () const {return Closer != "";}
+	// Checks if (*this) and ed form a matching encloser pair (such as () or [])
+	bool Matches (const OperatorData<T> &ed) const {return Closer == ed.GetName();}
 };
 
 template <class T = int>
 class PlusEqual : public OperatorData<T>
 {
 	public:
-	PlusEqual () : OperatorData ("+", 10) {}
-	~PlusEqual () {}
+	PlusEqual (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~PlusEqual () throw () {}
 
 	T &operator() (T &x) const {return x;}
 	T &operator() (T &x, T &y) const {return x += y;}
@@ -63,8 +97,8 @@ template <class T = int>
 class MinusEqual : public OperatorData<T>
 {
 	public:
-	MinusEqual () : OperatorData ("-", 10) {}
-	~MinusEqual () {}
+	MinusEqual (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~MinusEqual () throw () {}
 
 	T &operator() (T &x) const {x = -x; return x;}
 	T &operator() (T &x, T &y) const {return x -= y;}
@@ -74,8 +108,8 @@ template <class T = int>
 class TimesEqual : public OperatorData<T>
 {
 	public:
-	TimesEqual () : OperatorData ("*", 20) {}
-	~TimesEqual () {}
+	TimesEqual (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~TimesEqual () throw () {}
 
 	T &operator() (T &x, T &y) const {return x *= y;}
 };
@@ -84,8 +118,8 @@ template <class T = int>
 class DividesEqual : public OperatorData<T>
 {
 	public:
-	DividesEqual () : OperatorData ("/", 20) {}
-	~DividesEqual () {}
+	DividesEqual (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~DividesEqual () throw () {}
 
 	T &operator() (T &x, T &y) const {return x /= y;}
 };
@@ -94,8 +128,8 @@ template <class T = int>
 class ModEqual : public OperatorData<T>
 {
 	public:
-	ModEqual () : OperatorData ("MOD", 20) {}
-	~ModEqual () {}
+	ModEqual (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~ModEqual () throw () {}
 
 	T &operator() (T &x, T &y) const {return x %= y;}
 };
@@ -104,48 +138,51 @@ template <class T = int>
 class Period : public OperatorData<T>
 {
 	public:
-	Period () : OperatorData (".", 20) {}
-	~Period () {}
+	Period (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~Period () throw () {}
 };
 
 template <class T = int>
-class OpenBracket : public OperatorData<T>
+class OpenBracket : public EncloserData<T>
 {
 	public:
-	OpenBracket () : OperatorData ("[", 20) {}
-	~OpenBracket () {}
+	OpenBracket (const string &n, const string &c) throw () : EncloserData (n, c) {}
+	~OpenBracket () throw () {}
 };
 
 template <class T = int>
-class CloseBracket : public OperatorData<T>
+class CloseBracket : public EncloserData<T>
 {
 	public:
-	CloseBracket () : OperatorData ("]", 20) {}
-	~CloseBracket () {}
+	CloseBracket (const string &n) throw () : EncloserData (n) {}
+	~CloseBracket () throw () {}
 };
 
 template <class T = int>
-class OpenParenthesis : public OperatorData<T>
+class OpenParenthesis : public EncloserData<T>
 {
 	public:
-	OpenParenthesis () : OperatorData ("(", 20) {}
-	~OpenParenthesis () {}
+	OpenParenthesis (const string &n, const string &c) throw ()	: EncloserData (n, c) {}
+	~OpenParenthesis () throw () {}
+
+	// Unary prefix
+	T &operator() (T &x) const throw () {return x;}
 };
 
 template <class T = int>
-class CloseParenthesis : public OperatorData<T>
+class CloseParenthesis : public EncloserData<T>
 {
 	public:
-	CloseParenthesis () : OperatorData (")", 20) {}
-	~CloseParenthesis () {}
+	CloseParenthesis (const string &n) throw () : EncloserData (n, "") {}
+	~CloseParenthesis () throw () {}
 };
 
 template <class T = int>
 class And : public OperatorData<T>
 {
 	public:
-	And () : OperatorData ("AND", 20) {}
-	~And () {}
+	And (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~And () throw () {}
 
 	T &operator() (T &x, T &y) const {return x &= y;}
 };
@@ -154,8 +191,8 @@ template <class T = int>
 class Or : public OperatorData<T>
 {
 	public:
-	Or () : OperatorData ("OR", 20) {}
-	~Or () {}
+	Or (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~Or () throw () {}
 
 	T &operator() (T &x, T &y) const {return x |= y;}
 };
@@ -164,8 +201,8 @@ template <class T = int>
 class Xor : public OperatorData<T>
 {
 	public:
-	Xor () : OperatorData ("XOR", 20) {}
-	~Xor () {}
+	Xor (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~Xor () throw () {}
 
 	T &operator() (T &x, T &y) const {return x ^= y;}
 };
@@ -174,8 +211,8 @@ template <class T = int>
 class Not : public OperatorData<T>
 {
 	public:
-	Not () : OperatorData ("NOT", 20) {}
-	~Not () {}
+	Not (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~Not () throw () {}
 
 	T &operator() (T &x) const {x = ~x; return x;}
 };
@@ -184,8 +221,8 @@ template <class T = int>
 class ShiftLeft : public OperatorData<T>
 {
 	public:
-	ShiftLeft () : OperatorData ("SHL", 20) {}
-	~ShiftLeft () {}
+	ShiftLeft (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~ShiftLeft () throw () {}
 
 	T &operator() (T &x, T &y) const {return x <<= y;}
 };
@@ -194,8 +231,8 @@ template <class T = int>
 class ShiftRight : public OperatorData<T>
 {
 	public:
-	ShiftRight () : OperatorData ("SHR", 20) {}
-	~ShiftRight () {}
+	ShiftRight (const string &n, unsigned int p0, unsigned int p1) throw () : OperatorData (n, p0, p1) {}
+	~ShiftRight () throw () {}
 
 	T &operator() (T &x, T &y) const {return x >>= y;}
 };
@@ -208,19 +245,27 @@ class OperatorException : public exception
 	string WhatString;
 
 	public:
-	OperatorException (string s = "") : WhatString(s) {}
-	~OperatorException () {}
+	OperatorException (string s = "") throw () : WhatString(s) {}
+	~OperatorException () throw () {}
 
-	const char *what() const {return WhatString.c_str();}
+	const char *what() const throw () {return WhatString.c_str();}
 };
 
 // Operator hsa been used with wrong number or position of its arguments
 class InvalidArity : public OperatorException
 {
 	public:
-	InvalidArity (const string &s, const string &t) :
+	InvalidArity (const string &s, const string &t) throw () :
 		OperatorException (string ("Operator ") + s + " has no " + t + " form") {}
-	~InvalidArity () {}
+	~InvalidArity () throw () {}
+};
+
+// Precedence has been asked for an usage that does not exist
+class InvalidUsage : public OperatorException
+{
+	public:
+	InvalidUsage (unsigned long int u) throw () : OperatorException (string ("Invalid usage: ") + Print(u)) {}
+	~InvalidUsage () throw () {}
 };
 
 #endif
