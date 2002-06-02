12de37429955bd8b4899b323396f526778935a4b
helcio <helcio>
2002-06-02 14:30:27 +0000

    Improved type manipulation, but it´s very preliminar yet.

--- popasm/type.cpp
+++ popasm/type.cpp
@@ -1,6 +1,26 @@
+/***************************************************************************
+                            type.cpp  -  description
+                             -------------------
+    copyright            : (C) 2001 by Helcio Mello
+    email                : helcio@users.sourceforge.net
+ ***************************************************************************/
+
+//--------------------------------------------------------------------------
+// Control how types change among operations
+//--------------------------------------------------------------------------
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "type.h"
 
-Type &Type::operator+= (const Type &t)
+Type &Type::operator+= (const Type &t) throw ()
 {
 	switch (t.CurrentType)
 	{
@@ -32,57 +52,73 @@ Type &Type::operator+= (const Type &t)
 	return *this;
 }
 
-Type &Type::operator-= (const Type &t)
+Type &Type::operator-= (const Type &t) throw ()
 {
+	// += and -= share the same behavior
 	return *this += t;
 }
 
-Type &Type::operator*= (const Type &t)
+Type &Type::operator*= (const Type &t) throw (IncompatibleTypes)
 {
-	return *this += t;
-}
+	// Strong memory cannot multiply or divide.
+	if ((CurrentType == STRONG_MEMORY) || (t.CurrentType == STRONG_MEMORY)) throw IncompatibleTypes();
 
-Type &Type::operator/= (const Type &t)
-{
-	return *this += t;
-}
-
-Type &Type::operator%= (const Type &t)
-{
-	return *this += t;
-}
-
-Type &Type::operator&= (const Type &t)
-{
-	return *this += t;
-}
-
-Type &Type::operator|= (const Type &t)
-{
-	return *this += t;
-}
-
-Type &Type::operator^= (const Type &t)
-{
-	return *this += t;
-}
-
-Type &Type::operator<<= (const Type &t)
-{
-	return *this += t;
-}
-
-Type &Type::operator>>= (const Type &t)
-{
-	return *this += t;
-}
-
-Type Type::operator~ () const
-{
+	// The result of any valid operation will be scalar
+	CurrentType = SCALAR;
 	return *this;
 }
 
-Type Type::operator- () const
+Type &Type::operator/= (const Type &t) throw (IncompatibleTypes)
 {
+	// *= and /= share the same behavior
+	return *this *= t;
+}
+
+Type &Type::operator%= (const Type &t) throw (IncompatibleTypes)
+{
+	// *= and %= share the same behavior
+	return *this *= t;
+}
+
+Type &Type::operator&= (const Type &t) throw (IncompatibleTypes)
+{
+	// *= and &= share the same behavior
+	return *this *= t;
+}
+
+Type &Type::operator|= (const Type &t) throw (IncompatibleTypes)
+{
+	// *= and |= share the same behavior
+	return *this *= t;
+}
+
+Type &Type::operator^= (const Type &t) throw (IncompatibleTypes)
+{
+	// *= and ^= share the same behavior
+	return *this *= t;
+}
+
+Type &Type::operator<<= (const Type &t) throw (IncompatibleTypes)
+{
+	// *= and <<= share the same behavior
+	return *this *= t;
+}
+
+Type &Type::operator>>= (const Type &t) throw (IncompatibleTypes)
+{
+	// *= and >>= share the same behavior
+	return *this *= t;
+}
+
+Type Type::operator~ () const throw (IncompatibleTypes)
+{
+	// Strong memory cannot negate or complement.
+	if (CurrentType == STRONG_MEMORY) throw IncompatibleTypes();
 	return *this;
+}
+
+Type Type::operator- () const throw (IncompatibleTypes)
+{
+	// - and ~ share the same behavior
+	return ~*this;
 }
--- popasm/type.h
+++ popasm/type.h
@@ -1,5 +1,34 @@
+/***************************************************************************
+                            type.h  -  description
+                             -------------------
+    copyright            : (C) 2001 by Helcio Mello
+    email                : helcio@users.sourceforge.net
+ ***************************************************************************/
+
+//--------------------------------------------------------------------------
+// Control how types change among operations
+//--------------------------------------------------------------------------
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef TYPE_H
 #define TYPE_H
+
+#include <exception>
+
+class IncompatibleTypes : public exception
+{
+	public:
+	IncompatibleTypes () {}
+	~IncompatibleTypes () {}
+};
 
 class Type
 {
@@ -10,26 +39,26 @@ class Type
 	TypeName CurrentType;
 
 	public:
-	Type (TypeName t = SCALAR) : CurrentType (t) {}
-	~Type () {}
+	Type (TypeName t = SCALAR) throw () : CurrentType (t) {}
+	~Type () throw () {}
 
-	Type &operator+= (const Type &t);
-	Type &operator-= (const Type &t);
-	Type &operator*= (const Type &t);
-	Type &operator/= (const Type &t);
-	Type &operator%= (const Type &t);
+	Type &operator+= (const Type &t) throw ();
+	Type &operator-= (const Type &t) throw ();
+	Type &operator*= (const Type &t) throw (IncompatibleTypes);
+	Type &operator/= (const Type &t) throw (IncompatibleTypes);
+	Type &operator%= (const Type &t) throw (IncompatibleTypes);
 
-	Type &operator&= (const Type &t);
-	Type &operator|= (const Type &t);
-	Type &operator^= (const Type &t);
-	Type &operator<<= (const Type &t);
-	Type &operator>>= (const Type &t);
+	Type &operator&= (const Type &t) throw (IncompatibleTypes);
+	Type &operator|= (const Type &t) throw (IncompatibleTypes);
+	Type &operator^= (const Type &t) throw (IncompatibleTypes);
+	Type &operator<<= (const Type &t) throw (IncompatibleTypes);
+	Type &operator>>= (const Type &t) throw (IncompatibleTypes);
 
-	Type operator- () const;
-	Type operator~ () const;
+	Type operator- () const throw (IncompatibleTypes);
+	Type operator~ () const throw (IncompatibleTypes);
 
-	bool operator== (const Type &t) const {return CurrentType == t.CurrentType;}
-	bool operator!= (const Type &t) const {return CurrentType != t.CurrentType;}
+	bool operator== (const Type &t) const throw () {return CurrentType == t.CurrentType;}
+	bool operator!= (const Type &t) const throw () {return CurrentType != t.CurrentType;}
 };
 
 #endif
