78f05989ce91004c86f9cb8a3c99217b5552723f
helcio <helcio>
2002-06-02 14:27:12 +0000

    Added exception handling in method declarations.

--- popasm/register.cpp
+++ popasm/register.cpp
@@ -1,1 +1,21 @@
+/***************************************************************************
+                          register.cpp  -  description
+                             -------------------
+    copyright            : (C) 2001 by Helcio Mello
+    email                : helcio@users.sourceforge.net
+ ***************************************************************************/
+
+//--------------------------------------------------------------------------
+// Gathers data necessary to model registers
+//--------------------------------------------------------------------------
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "register.h"
--- popasm/register.h
+++ popasm/register.h
@@ -1,3 +1,23 @@
+/***************************************************************************
+                          register.h  -  description
+                             -------------------
+    copyright            : (C) 2001 by Helcio Mello
+    email                : helcio@users.sourceforge.net
+ ***************************************************************************/
+
+//--------------------------------------------------------------------------
+// Gathers data necessary to model registers
+//--------------------------------------------------------------------------
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef REGISTER_H
 #define REGISTER_H
 
@@ -11,10 +31,10 @@ class RegisterData
 	Byte Code;		// Register code
 
 	public:
-	RegisterData (const string &n, Byte s, Byte c) : Name (n), Size (s), Code (c) {}
-	~RegisterData () {}
+	RegisterData (const string &n, Byte s, Byte c) throw () : Name (n), Size (s), Code (c) {}
+	~RegisterData () throw () {}
 
-	const string &GetName () const {return Name;}
+	const string &GetName () const throw () {return Name;}
 };
 
 #endif
