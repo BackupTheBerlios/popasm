700a921aafb14460f96edde80b3b33613452b2bf
helcio <helcio>
2002-06-02 14:15:59 +0000

    Main module that now gets a file as argument and evaluates its first line.

--- popasm/main.cpp
+++ popasm/main.cpp
@@ -1,10 +1,14 @@
 /***************************************************************************
-                          main.cpp  -  description
+                          popasm.cpp  -  description
                              -------------------
-    begin                : Fri Sep 7 2001
-    copyright            : (C) 2001 by Helcio Mello
+    begin                : Tue May 28 2002
+    copyright            : (C) 2002 by Helcio Mello
     email                : helcio@users.sourceforge.net
  ***************************************************************************/
+
+//---------------------------------------------------------------------------
+// Main source file
+//---------------------------------------------------------------------------
 
 /***************************************************************************
  *                                                                         *
@@ -15,22 +19,71 @@
  *                                                                         *
  ***************************************************************************/
 
+#include <string>
 #include <iostream>
-#include "number.h"
+#include <vector>
+#include <exception>
 
-int main ()
+#include "inp_file.h"
+#include "lexical.h"
+#include "parser.h"
+#include "asmer.h"
+
+bool EndOfLine (Token *t)
 {
+	Token *tt = dynamic_cast<UnknownToken *> (t);
+	return (tt != 0);
+}
+
+string *ShowString (InputFile &inp)
+{
+	string *s;
+
+	cout << "Reading from line " << inp.GetCurrentLine() << endl;
+	s = inp.GetString();
+
+	cout << "String read: ";
+	if (*s == "\n")
+	{
+		cout << "End of line." << endl;
+	}
+	else
+	{
+		cout << *s << endl;
+	}
+
+	return s;
+}
+
+int main (int argc, char **argv)
+{
+	Assembler *a = new PopAsm();
+	if (argc != 2) return 1;
+	InputFile inp (argv[1]);
+
+	if (!inp)
+	{
+		cout << "File not found" << endl;
+	}
+
+	vector<Token *> v;
+	Token *t;
+
+	do
+	{
+		t = GetToken (inp);
+		if (EndOfLine (t)) break;
+		v.push_back (t);
+	} while (true);
+
 	try
 	{
-		NaturalNumber a(2);
-		NaturalNumber b("10_000");
-
-		cout << a.Power(b).Print () << endl;
+		Expression *e = Parser::EvaluateExpression (v);
+		e->Print();
+		delete e;
 	}
-	catch (NumberException ne)
-	{
-		cout << ne.what() << endl;
-	}
+	catch (exception &e) {cout << e.what() << endl;}
 
+	delete a;
 	return 0;
 }
