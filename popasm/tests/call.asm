c45e0f93a7241128d429400f41f59be4ad4a7db6
helcio <helcio>
2002-09-23 03:34:56 +0000

    Simple tests for CALL instruction.

---
+++ popasm/tests/call.asm
@@ -0,0 +1,10 @@
+call 1234h
+call dword 50h
+call ax
+call [bp]
+call 1234h:5678h
+call dword 1234h:56789ABCh
+call 1234h:dword 56789ABCh
+call [bx+si+34]
+call far [bx+si+34]
+call dword far [eax-12]
