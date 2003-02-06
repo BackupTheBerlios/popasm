e123347c9623a94f82197a31e679d897c3110517
helcio <helcio>
2003-02-06 02:58:17 +0000

    These files handle both constant and mutable contants.

---
+++ popasm/constant.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+                          constant.cpp  -  description
+                             -------------------
+    begin                : Sat Feb 1 2003
+    copyright            : (C) 2003 by Helcio Mello
+    email                : helcio@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "constant.h"
+
+void Constant::SetValue(Expression *NewValue) throw ()
+{
+	if (Mutable)
+	{
+		delete Value;
+		Value = NewValue;
+	}
+	else
+		throw RedefinedConstant (GetName());
+}
---
+++ popasm/constant.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+                          constant.h  -  description
+                             -------------------
+    begin                : Sat Feb 1 2003
+    copyright            : (C) 2003 by Helcio Mello
+    email                : helcio@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#include <exception>
+#include <string>
+
+#include "symbol.h"
+#include "type_exp.h"
+
+class RedefinedConstant : public exception
+{
+	string WhatString;
+
+	public:
+	RedefinedConstant (const string &s) throw () : WhatString ("Illegal constant redefinition: ") {WhatString += s;}
+	~RedefinedConstant () throw () {}
+
+	const char *what () const throw () {return WhatString.c_str();}
+};
+
+class Constant : public UserDefined
+{
+	Expression *Value;
+	bool Mutable;
+
+	public:
+	Constant (const string &n, Expression *exp = 0, bool m = false) throw () : UserDefined (n), Value(exp), Mutable(m) {}
+	~Constant () throw () {delete Value;}
+
+	const Expression *GetValue() const throw () {return Value;}
+	void SetValue(Expression *NewValue) throw ();
+
+	bool IsMutable () const throw () {return Mutable;}
+};
+
+#endif
