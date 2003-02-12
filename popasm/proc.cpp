b87b810a702245faece6cafddc5942b002e854f9
helcio <helcio>
2003-02-12 02:27:02 +0000

    These files handle procedures.

---
+++ popasm/proc.cpp
@@ -0,0 +1,18 @@
+/***************************************************************************
+                          proc.cpp  -  description
+                             -------------------
+    begin                : Sun Feb 9 2003
+    copyright            : (C) 2003 by Helcio Mello
+    email                : helcio@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "proc.h"
---
+++ popasm/proc.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+                          proc.h  -  description
+                             -------------------
+    begin                : Sun Feb 9 2003
+    copyright            : (C) 2003 by Helcio Mello
+    email                : helcio@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef PROC_H
+#define PROC_H
+
+#include <string>
+#include "variable.h"
+
+class Procedure : public UserDefined
+{
+	bool Open;
+
+	public:
+	Procedure (const string &n, int dist = NEAR) throw ()
+		: UserDefined(n, Type (0, WEAK_MEMORY, dist)), Open(true) {}
+	~Procedure () throw () {}
+
+	void Close () {if (!Open) throw 0; Open = false;}
+	bool IsOpen () const throw () {return Open;}
+};
+
+#endif
